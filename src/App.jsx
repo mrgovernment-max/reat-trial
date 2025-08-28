ab220e2ac2bcd5d009cb9152e23e15a8f86c8b31
Emman <efenteng1@gmail.com>
2025-08-28 16:10:22 +0100

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-body");
+    } else {
+      document.body.classList.remove("dark-body");
+    }
+  }, [darkMode]);
+
   return (
     <>
-      <div className="center-div">
-        <h1>HELLO</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia ea
-          pariatur nihil, quis eaque velit quia quasi ad repudiandae unde nulla
-          magnam reiciendis accusantium assumenda veniam fugiat culpa atque
-          quaerat.
-        </p>
+      <button className="theme-button" onClick={toggleTheme}>
+        {darkMode ? "dark" : "light"}
+      </button>
+      <div className={`center-div ${darkMode ? "add-bg-color" : ""}`}>
+        <h1 style={{ textAlign: "center" }}>HELLO</h1>
+        <input type="text" placeholder="Type something..." />
+        <button className="add-task-button">add task</button>
       </div>
     </>
   );

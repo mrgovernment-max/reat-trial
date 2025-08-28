5943768425ad99f5fd65395917f6180ab913b220
Emman <efenteng1@gmail.com>
2025-08-28 01:21:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="center-div">
+        <h1>HELLO</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia ea
+          pariatur nihil, quis eaque velit quia quasi ad repudiandae unde nulla
+          magnam reiciendis accusantium assumenda veniam fugiat culpa atque
+          quaerat.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
